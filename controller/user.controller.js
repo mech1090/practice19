85cc60f9f1daf3d5c10590c419f4dd559e817c19
Shahzad <mech1090@gmail.com>
2022-06-26 10:46:29 -0600

    done

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,9 +1,51 @@
+const {userValidation} = require('../validation/user.validation')
+const userService = require('../service/user.service')
+const bcrypt = require('bcrypt')
+const config = require('config')
+const { find } = require('../model/user.model')
 
 
 
-const getLoginForm = (req,res)={}
-const login = (req,res)={}
-const getSignupForm = (req,res)={}
-const signup = (req,res)={}
+const getLoginForm = (req,res)=>{
+    return res.render('login/layout')
+}
+const login = async(req,res)=>{
+    const {email,password} = req.body
+    const fields = {email,password}
+    const {error,value} = userValidation(fields)
+    if(error){
+        return res.render('login/layout',{message:error.details[0].message})
+    }
+    const findUser = await userService.getEmail({email})
+    if(!findUser){
+        return res.render('signup/layout',{message:'User does not Exist Signup HERE'})
+    }
+    const matchPassword = await bcrypt.compare(password,findUser.password)
+    if(!matchPassword){
+        return res.render('login/layout',{message:'Wrong Credentials'})
+    }
+    return res.render('user/layout')
+
+ }
+
+
+const getSignupForm = (req,res)=>{
+    return res.render('signup/layout')
+}
+const signup = async(req,res)=>{
+    const {email,password} = req.body
+    const fields = {email,password}
+    const {error,value} = userValidation(fields)
+    if(error){
+        return res.render('signup/layout',{message:error.details[0].message})
+    }
+    const findUser = await userService.getEmail({email})
+    if(findUser){
+        return res.render('login/layout',{message:'User Exist Login Here'})
+    }
+    const hashPassword = await bcrypt.hash(password,config.get('hash.salt'))
+    const createUser = await userService.createEntries({email,password:hashPassword})
+    return res.render('signup/layout',{message:'User Created'})
+}
 
 module.exports = {getLoginForm,login,getSignupForm,signup}
--- validation/user.validation.js
+++ validation/user.validation.js
@@ -1,12 +1,12 @@
 const Joi = require('joi')
 
-const userValidationSchema = (fields)=>{
-    const userValidation = Joi.object({
-        mail:Joi.string().min(8).max(24).required(),
-        password:Joi.string().min(6).max(24).required()
+const userValidation = (data)=>{
+    const userSchema = Joi.object({
+        email:Joi.string().min(8).max(32).required(),
+        password:Joi.string().min(6).max(32).required()
     })
-    const {error,value} = userValidation.validate(fields)
+    const {error,value} = userSchema.validate(data)
     return {error,value}
 }
 
-module.exports = {userValidationSchema}
+module.exports = {userValidation}
