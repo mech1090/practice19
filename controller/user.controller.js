5d6593be056335dec0a65fb88d5628437122f3ac
Shahzad <mech1090@gmail.com>
2022-06-26 10:20:58 -0600

    setup done

---
+++ controller/user.controller.js
@@ -0,0 +1,9 @@
+
+
+
+const getLoginForm = (req,res)={}
+const login = (req,res)={}
+const getSignupForm = (req,res)={}
+const signup = (req,res)={}
+
+module.exports = {getLoginForm,login,getSignupForm,signup}
